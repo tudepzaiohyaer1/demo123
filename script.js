5f348f9e722bb912428ecfa3c4cc5f3989d0f578
tudepzaiohyaer1 <tu200815@gmail.com>
2024-08-12 04:27:55 +0700

    uploade suorce code

---
+++ script.js
@@ -0,0 +1,39 @@
+let currentQuestion = 1;
+
+function checkAnswer(questionNumber, answer) {
+    const correctAnswers = {
+        1: 'a',
+        2: 'c',
+        3: 'a'
+    };
+
+    let buttons = document.querySelectorAll(`#question${questionNumber} button`);
+    buttons.forEach(button => {
+        button.classList.remove('shake', 'highlight');
+    });
+
+    if (answer === correctAnswers[questionNumber]) {
+        alert('Đúng rồi!');
+        if (questionNumber < 3) {
+            document.getElementById('question' + questionNumber).classList.add('slide-out');
+            setTimeout(function() {
+                document.getElementById('question' + questionNumber).style.display = 'none';
+                currentQuestion++;
+                document.getElementById('question' + currentQuestion).style.display = 'block';
+                document.getElementById('question' + currentQuestion).classList.add('slide-in');
+            }, 1000);
+        } else {
+            document.getElementById('question3').classList.add('slide-out');
+            setTimeout(function() {
+                document.getElementById('question3').style.display = 'none';
+                document.getElementById('message').style.display = 'block';
+                document.getElementById('message').classList.add('slide-in');
+                document.getElementById('image').style.display = 'block';
+            }, 1000);
+        }
+    } else {
+        document.querySelector(`#question${questionNumber} button[onclick*="${answer}"]`).classList.add('shake');
+    }
+}
+
+document.getElementById('question1').style.display = 'block';
